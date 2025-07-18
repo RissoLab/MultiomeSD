a74eaffa9c001d6f7845357c95da741b85836453
Dario <dario.righelli@gmail.com>
2025-07-18 15:44:15 +0200

    updating

--- set_ghpg.sh
+++ set_ghpg.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Assicurati di avere il sito aggiornato
-# quarto render made on docker
+# quarto render # made on docker
 
 # from qwebsite
-# Salva il branch corrente per tornarci dopo
 CURRENT_BRANCH=$(git branch --show-current)
 cp -r _site ../tmp_site
-# Evita Jekyll
 touch ../.nojekyll
 
-# Passa a gh-pages
-git checkout gh-pages
+if ! OUTPUT=$(git checkout gh-pages 2>&1); then
+  echo "ERROR: git checkout gh-pages failed"
+  echo "$OUTPUT"
+  exit 1
+fi
+
 cp -r ../tmp_site/* .
 
-# Commit e push
 git add .
 git commit -m "Aggiorna sito da $(date)"
 git push origin gh-pages
 
-# Torna al branch di partenza
 git checkout "$CURRENT_BRANCH"
